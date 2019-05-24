1ce38ce5ad91c17ec15d988802dfb5d8bbbff4c7
sakshi <sakshi.nj1201@gmail.com>
2019-05-24 11:42:07 +0530

    Initial commit

--- src/main/java/com/stackroute/Application.java
+++ src/main/java/com/stackroute/Application.java
@@ -12,21 +12,16 @@ public class Application
 {
     public static void main( String[] args )
     {
-/*        System.out.println("Using Bean Factory");
+       System.out.println("Using Bean Factory");
         Resource resource=new ClassPathResource("beans.xml");
         BeanFactory factory=new XmlBeanFactory(resource);
         Movie movie=(Movie)factory.getBean("movie");
-        System.out.println(movie.getActor().getName()+" Acted in "+movie.getMovieName());*/
+        System.out.println(movie.getActor().getName()+" Acted in "+movie.getMovieName());
 
         System.out.println("Using Application Context");
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1=context.getBean("movie",Movie.class);
-        System.out.println(movie1.getActor().getName()+" Acted in "+movie1.getMovieName());
+        System.out.println(movie1.getActor().getName()+" Acted in "+movie.getMovieName());
 
-        //testing bean scope
-        ApplicationContext context1=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2=context1.getBean("movie",Movie.class);
-        Movie movie3=context1.getBean("movie",Movie.class);
-        System.out.println(movie2==movie3);
     }
 }
