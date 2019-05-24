42d02ba5f1a04b92fcf5ca2835df16b4d54ed77d
sakshi <sakshi.nj1201@gmail.com>
2019-05-24 12:40:16 +0530

    added scope

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -9,6 +9,11 @@ public class Movie {
     public Movie() {
     }
 
+    public Movie(int id, String movieName) {
+        this.id = id;
+        this.movieName = movieName;
+    }
+
     public Movie(int id, String movieName, Actor actor) {
         this.id = id;
         this.movieName = movieName;
